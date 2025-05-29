d522dfb4b27819310a7b38a64654c056e5f1c1af
Shaline Davida <shalinedavida@gmail.com>
2025-05-29 21:56:43 +0300

    Added pseudocode to the files

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 
 //1. You are building a system that sends email reminders to users 5 seconds after they register. 
 // Create an async function sendReminder(email) that waits 5 seconds using setTimeout and then logs "Reminder sent to [email]". Simulate sending a reminder to 3 users.
+
+                    // Pseudocode
+// 1.Define  a function sendReminder that takes in a list as a parameter.
+// 2.
 function sendReminder(emails) {
         return new Promise((resolve)=>{
             setTimeout(() => {
@@ -23,6 +27,12 @@ sendReminderAsync();
 // // The first two attempts fail, but the third succeeds. Write a function tryLogin() that uses a counter and Promises. 
 // // Use setTimeout to simulate a 1-second delay between attempts. Log "Login successful" or "Login failed after 3 attempts" based on whether login succeeds.
 
+              // Pseudocode
+  // 1.Define a function tryLogin()
+  // 2.Initialize a counter for the attempts to begin at zero.
+  // 3.Create a function that simulates a  login attempt.In the login attempts simulate a delay .
+  // 4.Increase  the attempt counter and if the attempt number is strictly equal to 3 indicate that the login is a successs otherwise is has failed.
+  // 5.If failed and attempts is less than 3 retry until the last attempt to print its a success.            // 
 function tryLogin() {
   let attempts = 0;
 
@@ -60,7 +70,12 @@ tryLogin();
 
 //3. Build a countdown timer that counts down from 5 to 0, displaying one number per second using setInterval.
 //  When the countdown reaches 0, stop the interval and log "Time's up!".
-
+                  // Pseudocode
+  // 1. Define a variable countdownTimer .
+  // 2.Start the countdown at 5 
+  // 3.After every second display the current number and then decrease it by one until the countdown get to zero
+  // 4.Print out 'Times up' to indicate the end of the countdown.
+             
 const countdownTimer = () =>{
   let count = 5;
  const interval = setInterval(() => {
@@ -79,6 +94,9 @@ countdownTimer();
 //4. You are simulating a page that loads data in stages. 
 // Create an async function called loadPage() that: logs "Loading header...", waits 1 second; logs "Loading content...", waits 2 seconds; logs "Loading footer...", 
 // waits 1 second; and finally logs "Page fully loaded". Use setTimeout inside Promises and await them in sequence.
+
+                      //  Pseudocode
+  // 1.Define a function loadPage that handles  the stage loading                     
 async function loadPage() {
   function awaitMessages(seconds,messages){
     return new Promise((resolve)=>
@@ -99,6 +117,8 @@ loadPage();
 // 5.You are simulating fetching stock prices with delays. 
 // Write a function fetchPrice(symbol) that returns a Promise which resolves after 2 seconds with the message "Price for [symbol] retrieved". 
 // Use async/await to call it for two different stocks ("AAPL" and "GOOG") and log the messages in order.
+
+                  //  Pseudocode
 function fetchPrice(symbol) {
   return new Promise((resolve) => {
     setTimeout(() => {
